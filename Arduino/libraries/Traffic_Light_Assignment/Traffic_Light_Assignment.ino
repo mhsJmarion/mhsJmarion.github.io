54f718d94a82e17abc36988d4a1288f067937b54
mhsJmarion <43574843+mhsJmarion@users.noreply.github.com>
2018-12-17 09:23:30 -0500

    Add files via upload

--- Arduino/libraries/Traffic_Light_Assignment/Traffic_Light_Assignment.ino
+++ Arduino/libraries/Traffic_Light_Assignment/Traffic_Light_Assignment.ino
@@ -8,13 +8,12 @@ pinMode(12,OUTPUT);
 
 void loop() {
 digitalWrite(12,HIGH);
-digitalWrite(10,HIGH);
 delay(2000);
-digitalWrite(11,HIGH);
-digitalWrite(12,HIGH);
-delay(1500);
+digitalWrite(12,LOW);
 digitalWrite(10,HIGH);
-digitalWrite(11,HIGH);
+delay(1500);
+digitalWrite(10,LOW);
+digitalWrite(12,HIGH);
 delay(1000);
 
 }
